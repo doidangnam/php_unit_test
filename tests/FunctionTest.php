74a2afff9959b810ffd8ac4eb432b4a58e8c73ad
namdoi <doidangnam058@gmail.com>
2022-03-11 14:25:34 +0700

    [Update] Final

--- tests/FunctionTest.php
+++ tests/FunctionTest.php
@@ -2,18 +2,18 @@
 
 use PHPUnit\Framework\TestCase;
 
-class FunctionTest extends TestCase 
+class FunctionTest extends TestCase
 {
     public function testAddReturnsTheCorrectSum()
     {
         require 'functions.php';
 
-        $this->assertEquals(4, add(2,2));
-        $this->assertEquals(8, add(3,5));
+        $this->assertEquals(4, add(2, 2));
+        $this->assertEquals(8, add(3, 5));
     }
 
-    public function testDoesNotReturnTheCorrectSum() 
+    public function testAddDoesNotReturnTheIncorrectSum()
     {
-        $this->assertNotEquals(5, add(2,2));
+        $this->assertNotEquals(5, add(2, 2));
     }
 }
--- tests/UserTest.php
+++ tests/UserTest.php
@@ -4,32 +4,39 @@ use PHPUnit\Framework\TestCase;
 
 class UserTest extends TestCase
 {
-    public function testReturnFullName()
+
+    public function testReturnsFullName()
     {
         $user = new User;
 
-        $user->first_name = "Nam";
-        $user->surname = "Doi";
+        $user->first_name = "Teresa";
+        $user->surname = "Green";
 
-        $this->assertEquals("Nam Doi", $user->getFullName());
+        $this->assertEquals('Teresa Green', $user->getFullName());
     }
 
-    public function testtFullNameIsEmptyByDefault()
+    public function testFullNameIsEmptyByDefault()
     {
         $user = new User;
 
         $this->assertEquals('', $user->getFullName());
     }
     
-    /**
-     * @test
-     */
-    public function user_has_first_name()
+    public function testNotificationIsSent()
     {
         $user = new User;
-
-        $user->first_name = "Mio";
-
-        $this->assertEquals('Mio', $user->first_name);
-    }
+        
+        $mock_mailer = $this->createMock(Mailer::class);
+        
+        $mock_mailer->expects($this->once())
+                    ->method('sendMessage')
+                    ->with($this->equalTo('dave@example.com'), $this->equalTo('Hello'))
+                    ->willReturn(true);        
+                
+        $user->setMailer($mock_mailer);
+                        
+        $user->email = 'dave@example.com';
+        
+        $this->assertTrue($user->notify("Hello"));
+    }    
 }
