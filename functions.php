d3837b90664a7d84ee92ca0c3fb4b79cbd353f2f
namdoi <doidangnam058@gmail.com>
2022-03-11 10:10:42 +0700

    First Commit

---
+++ functions.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Return the sum of the two numbers
+ *
+ * @param  integer $a The first number
+ * @param  integer $b The second number
+ * @return integer
+ */
+function add($a, $b) {
+    return $a + $b;
+}
